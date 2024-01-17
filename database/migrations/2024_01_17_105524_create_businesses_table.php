8ae0bffe6003250c0898102a055dd955c33baaf4
Ezra <est@webmecanik.com>
2024-01-17 12:10:43 +0100

    Linked tables to Business model

---
+++ database/migrations/2024_01_17_105524_create_businesses_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('businesses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('address');
+            $table->string('postal_code');
+            $table->string('city');
+            $table->bigInteger('siret');
+            $table->unsignedBigInteger('craft_id');
+            $table->unsignedBigInteger('specialty_id');
+            $table->string('website');
+            $table->string('biography');
+            $table->string('history');
+            $table->unsignedBigInteger('theme_id');
+            $table->timestamps();
+            $table->foreign('craft_id')
+                ->references('id')
+                ->on('crafts')
+                ->onDelete('cascade');
+            $table->foreign('specialty_id')
+                ->references('id')
+                ->on('specialties')
+                ->onDelete('cascade');
+            $table->foreign('theme_id')
+                ->references('id')
+                ->on('themes')
+                ->onDelete('cascade');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('businesses');
+    }
+};
