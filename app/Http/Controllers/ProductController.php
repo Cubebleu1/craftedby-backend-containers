990894985dc66f3abffc3dec6c98b5974566bd86
Ezra <est@webmecanik.com>
2024-01-19 14:21:47 +0100

    Product CRUD

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+    public function show($id): JsonResponse
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+        return response()->json($product);
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $validatedData = $request->validate([
+            // Define validation rules
+        ]);
+
+        $product = Product::create($validatedData);
+        return response()->json($product, 201);
+    }
+
+    public function update(Request $request, $id): JsonResponse
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        $validatedData = $request->validate([
+            // Define  validation rules
+        ]);
+
+        $product->update($validatedData);
+        return response()->json($product);
+    }
+
+    public function destroy($id): JsonResponse
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        $product->delete();
+        return response()->json(['message' => 'Product deleted successfully']);
+    }
 }
